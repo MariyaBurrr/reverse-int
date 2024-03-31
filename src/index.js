333f879ea85c12a20b91992d66b59e7f2e2dd3ea
Maria Burnina <izoprenmari@gmail.com>
2024-03-31 17:57:35 +0500

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+// module.exports = 
+
+function reverse (n) {
+    result = '';
+    const number = String(n);
+
+    for (let i = number.length - 1; i >= 0; i--) {
+        result = result + number[i];
+    }
+    return result;
 }
+
+const flower = reverse(549);
+console.log(flower);
